38c8bdc1a36a4e41afff8a95afa91422d69d5a27
Luo jie <jluo@thoughtworks.com>
2020-01-14 16:38:58 +0800

    Refactor: remove switch statement

--- src/main/java/com/thoughtworks/CommandResolver.java
+++ src/main/java/com/thoughtworks/CommandResolver.java
@@ -6,10 +6,19 @@ import com.thoughtworks.model.Location;
 import com.thoughtworks.utils.FileLoader;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CommandResolver {
+
+    private static final Map<String, Function<Context, Command>> commandMap = Map.of(
+            "P", PrintCommand::new,
+            "M", MoveCommand::new,
+            "L", TurnLeftCommand::new,
+            "R", TurnRightCommand::new
+    );
 
     public List<Command> resolve(String fileName) {
         List<String> inputs = FileLoader.loadFile(fileName);
@@ -18,13 +27,10 @@ public class CommandResolver {
             throw new RuntimeException("Missing input command");
         }
 
-        String[] dimensions = inputs.get(0).split(" ");
-        Location startLocation = new Location(Integer.valueOf(dimensions[0]), Integer.valueOf(dimensions[1]));
-        Direction startDirection = Direction.parseFromValue(dimensions[2]);
-        Context context = new Context(startLocation, startDirection);
+        Context context = resolveContext(inputs.get(0));
 
         List<Command> commandList = inputs.subList(1, inputs.size()).stream()
-                .map(input -> parseCommand(input, context))
+                .map(input -> resolveCommand(input, context))
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
 
@@ -32,13 +38,17 @@ public class CommandResolver {
         return commandList;
     }
 
-    private Command parseCommand(String input, Context context) {
-        switch (input) {
-            case "P": return new PrintCommand(context);
-            case "M": return new MoveCommand(context);
-            case "L": return new TurnLeftCommand(context);
-            case "R": return new TurnRightCommand(context);
-            default: return null;
+    private Context resolveContext(String input) {
+        String[] dimensions = input.split(" ");
+        Location startLocation = new Location(Integer.valueOf(dimensions[0]), Integer.valueOf(dimensions[1]));
+        Direction startDirection = Direction.parseFromValue(dimensions[2]);
+        return new Context(startLocation, startDirection);
+    }
+
+    private Command resolveCommand(String input, Context context) {
+        if (!commandMap.containsKey(input)) {
+            throw new RuntimeException(String.format("Unknown Command: %s", input));
         }
+        return commandMap.get(input).apply(context);
     }
 }
